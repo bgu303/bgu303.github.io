fa3b09d9194658acd2566d516c08eb71d0043540
bgu303 <90905706+bgu303@users.noreply.github.com>
2022-08-08 02:05:43 +0300

    Add files via upload

--- salarycounter/script.js
+++ salarycounter/script.js
@@ -6,20 +6,35 @@ function start() {
     let minutesInput = document.getElementById("minutes").value;
     let totalSeconds = hoursInput * 60 * 60 + minutesInput * 60
     var hourlySalary = document.getElementById("hourly-salary").value;
-    var secondlySalary = hourlySalary / 60 / 60
+    var secondlySalary = hourlySalary / 60 / 60;
+
+    if (document.getElementById("hours").value.length == 0 && document.getElementById("minutes").value.length == 0) {
+        alert("Add hours and/or minutes!");
+        return;
+    }
+
+    if (document.getElementById("hours").value < 0 || document.getElementById("minutes").value < 0 || document.getElementById("hourly-salary").value < 0) {
+        alert("No negative values please!");
+        return;
+    }
+
+    if (document.getElementById("hourly-salary").value.length == 0) {
+        alert("Add hourly salary!");
+        return;
+    }
 
     intervalId = setInterval(function() {
         let hours = Math.floor(totalSeconds / 3600);
         var minutes = Math.floor(totalSeconds % 3600 / 60);
         var seconds = Math.floor(totalSeconds % 3600 % 60);
-        document.getElementById("stopwatch-timer").innerHTML = "Time till freedom: " + hours + ":" + minutes + ":" + seconds
-        totalSalary += secondlySalary
-        document.getElementById("salary").innerHTML = "Money made so far: " + totalSalary.toFixed(5) + " €"
-        totalSeconds--
+        document.getElementById("stopwatch-timer").innerHTML = "Time till freedom: " + hours + ":" + minutes + ":" + seconds;
+        totalSalary += secondlySalary;
+        document.getElementById("salary").innerHTML = "Money made so far: " + totalSalary.toFixed(5) + " €";
+        totalSeconds--;
 
         if (totalSeconds == 0) {
-            document.getElementById("stopwatch-timer").innerHTML = "FREEEEEEEEDOOOOOOOOOOOOOOOM"
-            clearInterval(intervalId)
+            document.getElementById("stopwatch-timer").innerHTML = "FREEEEEEEEDOOOOOOOOOOOOOOOM";
+            clearInterval(intervalId);
         }
     }, 1000)
     document.getElementById("button-start").disabled = true;
@@ -38,7 +53,7 @@ function start() {
 } */
 
 function stop() {
-    clearInterval(intervalId)
+    clearInterval(intervalId);
 }
 
 function resume() {
